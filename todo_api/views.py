40cf87ae647309608ed9865304f5ed224a79ba58
nithindaniel1 <nithindaniel2018@gmail.com>
2023-05-08 11:11:33 +0530

    added update function

--- todo_api/views.py
+++ todo_api/views.py
@@ -50,4 +50,27 @@ class TodoListApiView(APIView):
 
         serializer = TodoSerializer(todo_instance)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+      # 4. Update
+    def put(self, request, todo_id, *args, **kwargs):
+        '''
+        Updates the todo item with given todo_id if exists
+        '''
+        todo_instance = self.get_object(todo_id, request.user.id)
+        if not todo_instance:
+            return Response(
+                {"res": "Object with todo id does not exists"}, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        data = {
+            'task': request.data.get('task'), 
+            'completed': request.data.get('completed'), 
+            'user': request.user.id
+        }
+        serializer = TodoSerializer(instance = todo_instance, data=data, partial = True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
