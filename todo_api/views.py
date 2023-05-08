6c4f3794e67e0309ab8b7133473e71fa4d6c2adc
nithindaniel1 <nithindaniel2018@gmail.com>
2023-05-08 11:15:52 +0530

    changed todo detail view

--- todo_api/views.py
+++ todo_api/views.py
@@ -36,7 +36,20 @@ class TodoListApiView(APIView):
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-     # 3. Retrieve
+class TodoDetailApiView(APIView):
+    # add permission to check if user is authenticated
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self, todo_id, user_id):
+        '''
+        Helper method to get the object with given todo_id, and user_id
+        '''
+        try:
+            return Todo.objects.get(id=todo_id, user = user_id)
+        except Todo.DoesNotExist:
+            return None
+
+    # 3. Retrieve
     def get(self, request, todo_id, *args, **kwargs):
         '''
         Retrieves the Todo with given todo_id
@@ -50,8 +63,8 @@ class TodoListApiView(APIView):
 
         serializer = TodoSerializer(todo_instance)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    
-      # 4. Update
+
+    # 4. Update
     def put(self, request, todo_id, *args, **kwargs):
         '''
         Updates the todo item with given todo_id if exists
@@ -72,8 +85,8 @@ class TodoListApiView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-     # 5. Delete
+
+    # 5. Delete
     def delete(self, request, todo_id, *args, **kwargs):
         '''
         Deletes the todo item with given todo_id if exists
